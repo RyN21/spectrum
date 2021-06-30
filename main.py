d6d2cbf0492f1f06b5af552df3fce99b39bdb9d2
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-30 17:44:13 -0600

    Add route to get csv file and return a table

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from collections import Counter
-import requests
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import HTMLResponse
+import requests
 import csv
+import pandas as pd
 
 
 app = FastAPI()
@@ -11,14 +13,14 @@ app = FastAPI()
 # CREATE RELEASE JSON FROM API REQUEST
 r = requests.get("https://www.energy.gov/sites/prod/files/2020/12/f81/code-12-15-2020.json")
 json = r.json()
-releases = json["releases"]
+json_releases = json["releases"]
 
 # WRITE CSV FILE
 with open('releases.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['organization', 'labor_hours', 'status', 'licenses', 'date_created'])
 
-    for x in releases:
+    for x in json_releases:
         licenses = x['permissions']['licenses'][0]['name'] if  x['permissions']['licenses'] != [] else 'N/A'
 
         writer.writerow([x['organization'], x['laborHours'], x['status'], licenses, x['date']['created']])
@@ -27,8 +29,6 @@ with open('releases.csv', 'w', newline='') as file:
 # FILL DATABASE FROM CSV FILE
 # IN MEMORY DATABASE
 db = {}
-
-
 
 with open('releases.csv', 'r') as file:
     reader = csv.reader(file)
@@ -105,12 +105,29 @@ def get_releases_sorted_by_labor_hours():
     json_object = jsonable_encoder({'organization': sorted_by_labor_hours})
     return json_object
 
+@app.get('/releases/csv', response_class=HTMLResponse)
+async def get_release_csv():
+    # WRITE CSV FILE
+    with open('releases_new.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['organization', 'release_count', 'total_labor_hours', 'all_in_production', 'licenses', 'most_active_months'])
+
+        for x in releases:
+            writer.writerow([x['organization'], x['release_count'], x['total_labor_hours'], x['all_in_production'], x['licenses'], x['most_active_months']])
+
+    with open('releases_new.csv', 'r') as file:
+        read_releases = csv.reader(file, delimiter=' ')
+
+        for row in read_releases:
+            print(', '.join(row))
+
+    a = pd.read_csv("releases_new.csv")
+    a.to_html("Table.html")
+    html_file = a.to_html()
+
+    return html_file
 
 
-
-
-# Remove status from json
-# Add Restful API that retruns data in CSV format
 # Add Readme
 # Provide live URL of hosted API
 # dockerize API
